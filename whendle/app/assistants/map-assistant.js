09d27311f62f81c10cdec556c9231c2a7bf1b24b
Dylan Meissner <dylanmei@gmail.com>
2010-02-07 09:37:41 -0800

    map background color same as map

--- whendle/app/assistants/map-assistant.js
+++ whendle/app/assistants/map-assistant.js
@@ -7,7 +7,7 @@ MapAssistant = Class.create(Whendle.Gallery.View, {
 		var profile = Whendle.profile();
 		profile.set('gallery', 'map');
 	},
-	
+
 	setup: function() {
 
 		this.model = {
@@ -17,54 +17,52 @@ MapAssistant = Class.create(Whendle.Gallery.View, {
 		this.setup_menus();
 		this.setup_widgets();
 		this.attach_events();
-		
+
 		this.fire(Whendle.Event.loading);
 	},
-	
+
 	setup_menus: function() {
 		var menu = {};
 		Object.extend(menu, StageAssistant.Gallery_menu);
 		menu.attributes.menuClass = 'no-fade';
-//		menu.model.visible = false;
 		menu.model.items[0].toggleCmd = 'map';
 		this.controller.setupWidget(Mojo.Menu.commandMenu, menu.attributes, menu.model);
 	},
-	
+
 	setup_widgets: function() {
 		this.growler = this.controller.get('growler');
 		this.controller.setupWidget(this.growler.id, {});
 		this.setup_map();
 	},
-	
+
 	setup_map: function() {
 		this.map = this.controller.get('map');
-
 		var profile = Whendle.profile();
 		var location = profile.get('location');
 		var attributes = location ? { longitude: location.x, latitude: location.y } : undefined;
 		this.controller.setupWidget(this.map.id, attributes);
 	},
-	
+
 	attach_events: function() {
 		this.controller.listen(this.growler.id, Mojo.Event.tap,
 			this.growler.tap_handler = this.on_growler_tapped.bind(this));
 		this.controller.listen(this.map.id, ':location',
 			this.map.location_handler = this.on_location_changed.bind(this));
 	},
-	
+
 	loaded: function(event) {
 		this.is_loaded = true;
 		if (this.report_error(event.error)) return;
 
 		var profile = Whendle.profile();
 		var location = profile.get('location');
-		
+
 		var now = event.now;
 		var map = this.map;
-		
+
 		map.mojo.sun(now.declination, now.hour_angle);
 		map.mojo.draw();
-		
+
 		var clocks = event.clocks;
 		for (var i = 0; i < clocks.length; i++) {
 			var clock = clocks[i];
@@ -77,28 +75,28 @@ MapAssistant = Class.create(Whendle.Gallery.View, {
 
 		map.mojo.flick(location);
 	},
-	
+
 	added: function(event) {
 		if (this.report_error(event.error)) return;
-		
+
 		var clock = event.clocks[0];
 		var coordinate = { x: clock.longitude, y: clock.latitude };
 		map.mojo.mark(clock.id, clock.title.escapeHTML(), clock.time, coordinate);
 		map.mojo.flick(coordinate);
-		
+
 		this.growler.mojo.dismiss();
 	},
-	
+
 	removed: function(event) {
 	},
-	
+
 	changed: function(event) {
 		var now = event.now;
 		var map = this.map;
-		
+
 		map.mojo.sun(now.declination, now.hour_angle);
 		map.mojo.draw();
-		
+
 		event.clocks.each(function(c) {
 			var coordinate = { x: c.longitude, y: c.latitude };
 			map.mojo.mark(c.id, c.title.escapeHTML(), c.time, coordinate);
@@ -133,16 +131,16 @@ MapAssistant = Class.create(Whendle.Gallery.View, {
 				orientation = 'right';
 				break;
 		}
-		
+
 		if (orientation.blank()) return;
 
 		var stage_controller = this.controller.stageController;
 		if (orientation == stage_controller.getWindowOrientation()) return;
-		
+
 		stage_controller.setWindowOrientation(orientation);
 		this.map.mojo.orient(orientation);
 	},
-	
+
 	on_location_changed: function(event) {
 		var profile = Whendle.profile();
 		profile.set('location', event.location);
@@ -152,6 +150,8 @@ MapAssistant = Class.create(Whendle.Gallery.View, {
 	},
 
 	activate: function(place) {
+		this.controller.sceneElement.up('body').addClassName('map-body');
+
 		this.controller.setMenuVisible(Mojo.Menu.commandMenu, true);
 		this.controller.listen(document, 'orientationchange',
 			this.orientation_handler = this.on_orientation_changed.bind(this));
@@ -168,16 +168,17 @@ MapAssistant = Class.create(Whendle.Gallery.View, {
 			this.fire(Whendle.Event.loading);
 		}
 	},
-	
+
 	deactivate: function() {
+		this.controller.sceneElement.up('body').removeClassName('map-body');
 		this.controller.stopListening(document,
 			'orientationchange', this.orientation_handler);
 	},
-	
+
 	cleanup: function() {
 		this.detach_events();
 	},
-	
+
 	detach_events: function() {
 		this.controller.stopListening(this.growler, Mojo.Event.tap, this.growler.tap_handler);
 		this.controller.stopListening(this.map, ':location', this.map.location_handler);
