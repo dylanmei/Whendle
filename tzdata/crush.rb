d94ba800d89286ea93f65b2f5dfe478aa7f8c86b
dylanmei <dylanmei@gmail.com>
2010-11-11 10:43:48 -0800

    Add crush script for tzdata files

---
+++ tzdata/crush.rb
@@ -0,0 +1,47 @@
+# encoding: utf-8
+
+class String
+  def blank?
+    self !~ /\S/
+  end
+end
+
+def timezone_file?(f)
+  if f == "." or f == ".."
+    false
+  else
+    File.extname(f).empty?
+  end
+end
+
+def strip_comments(line)
+  line = "" unless line.valid_encoding?
+  line.sub!(/#.*$/, "") unless line.empty?
+  line
+end
+
+source = ARGV[0]
+destination = ARGV[1]
+
+if source.nil?
+  puts "usage: crush.rb <source-directory> <destination-directory>"
+else
+  destination = "." if destination.nil?
+
+  Dir.foreach(source) do |f|
+    next unless timezone_file?(f)
+
+    f1 = File.new(File.join(source, f), 'r')
+    f2 = File.new(File.join(destination, f), 'w')
+
+    f1.each do |l|
+      line = strip_comments l
+      next if line.blank?
+      f2.print line
+    end
+
+    f1.close
+    f2.close
+  end
+end
+
